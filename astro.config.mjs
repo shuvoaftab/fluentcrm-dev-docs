83329dcbab0aafd25fcd9c6c6417a55cce98d330
Ibrahim Sharif <shuvoaftab@gmail.com>
2024-07-10 17:35:14 +0600

    ADDED: domain to publish

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,6 +3,7 @@ import starlight from '@astrojs/starlight';
 
 // https://astro.build/config
 export default defineConfig({
+	site: 'http://fluentcrm.top',
 	integrations: [
 		starlight({
 			title: 'FluentCRM Docs',
