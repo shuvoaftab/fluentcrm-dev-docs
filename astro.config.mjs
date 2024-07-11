a60e55d5f4bb7b14fb40744e7252a79823e42918
Ibrahim Sharif <8376586+shuvoaftab@users.noreply.github.com>
2024-07-11 22:31:56 +0600

    ADD: Free Actions by Arif

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,57 +1,57 @@
-import starlight from "@astrojs/starlight";
-import { defineConfig } from "astro/config";
+import { defineConfig } from 'astro/config';
+import starlight from '@astrojs/starlight';
 
 // https://astro.build/config
 export default defineConfig({
-  site: "http://fluentcrm.top",
-  integrations: [
-    starlight({
-      title: "FluentCRM Docs",
-      favicon: "./src/assets/favicon/favicon.ico",
-      // social: {
-      // 	github: 'https://github.com/shuvoaftab/fluentcrm-dev-docs',
-      // },
-      customCss: [
-        // Relative path to your custom CSS file
-        "./src/styles/custom.css",
-      ],
-      sidebar: [
-        {
-          label: "Get Started",
-          items: [
-            // Each item here is one entry in the navigation menu.
-            { label: "Start", slug: "index/start" },
-          ],
-        },
-        {
-          label: "Settings",
-          autogenerate: { directory: "settings" },
-        },
-        {
-          label: "Contacts",
-          autogenerate: { directory: "contacts" },
-        },
-        {
-          label: "Company",
-          autogenerate: { directory: "company" },
-        },
-        {
-          label: "Emails",
-          autogenerate: { directory: "emails" },
-        },
-        {
-          label: "Automation",
-          autogenerate: { directory: "automation" },
-        },
-        {
-          label: "Miscellaneous",
-          autogenerate: { directory: "misc" },
-        },
-        {
-          label: "Glossary",
-          autogenerate: { directory: "glossary" },
-        },
-      ],
-    }),
-  ],
+	site: 'http://fluentcrm.top',
+	integrations: [
+		starlight({
+			title: 'FluentCRM Docs',
+			favicon: './src/assets/favicon/favicon.ico',
+			// social: {
+			// 	github: 'https://github.com/shuvoaftab/fluentcrm-dev-docs',
+			// },
+			customCss: [
+				// Relative path to your custom CSS file
+				'./src/styles/custom.css',
+			],
+			sidebar: [
+				{
+					label: 'Get Started',
+					items: [
+						// Each item here is one entry in the navigation menu.
+						{ label: 'Start', slug: 'index/start' },
+					],
+				},
+				{
+					label: 'Settings',
+					autogenerate: { directory: 'settings' },
+				},
+				{
+					label: 'Contacts',
+					autogenerate: { directory: 'contacts' },
+				},
+				{
+					label: 'Company',
+					autogenerate: { directory: 'company' },
+				},
+				{
+					label: 'Emails',
+					autogenerate: { directory: 'emails' },
+				},
+				{
+					label: 'Automation',
+					autogenerate: { directory: 'automation' },
+				},
+				{
+					label: 'Miscellaneous',
+					autogenerate: { directory: 'misc' },
+				},
+				{
+					label: 'Glossary',
+					autogenerate: { directory: 'glossary' },
+				},
+			],
+		}),
+	],
 });
